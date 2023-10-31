a5053d36edbc66750f1f6f252206028ae40e1b41
Amir Zakirov <amurzakirov@gmail.com>
2023-10-31 22:27:00 +0300

    initial commit

---
+++ Dz_1.py
@@ -0,0 +1,13 @@
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
